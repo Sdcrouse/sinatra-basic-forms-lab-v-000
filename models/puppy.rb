369f86d0d725030a54266b973077f1c40152d020
Steven Crouse <sdcrouse91@gmail.com>
2019-07-01 03:15:58 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,8 +1,9 @@
 class Puppy
   attr_accessor :name, :breed, :months_old
-  alias_method :age, :months_old # Nice to know you can do this!
+  alias_method :age, :months_old
   alias_method :age=, :months_old=
   # alias_method :new_method_name, :original_method_name
+  # Nice to know that you can do this! It makes the "post '/"
   
   def initialize(puppy_attributes)
     puppy_attributes.each do |key, value|
