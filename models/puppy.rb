f9d6e9949af48f3c8a416446a46b9522aa4039e2
Steven Crouse <sdcrouse91@gmail.com>
2019-07-01 03:14:21 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,7 +1,8 @@
 class Puppy
   attr_accessor :name, :breed, :months_old
-  alias_method :age, :months_old
+  alias_method :age, :months_old # Nice to know you can do this!
   alias_method :age=, :months_old=
+  # alias_method :new_method_name, :original_method_name
   
   def initialize(puppy_attributes)
     puppy_attributes.each do |key, value|
